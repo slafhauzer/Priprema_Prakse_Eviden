ca0570aa40e832f3371e24a241820a08a3c71db1
jakov.slafhauzer <slafhauzerjakov@gmail.com>
2025-12-17 09:32:37 +0100

    Added navigation bar to Frontend

--- Frontend/PripremaPrakse_JakovSlafhauzer/src/app/app.routes.ts
+++ Frontend/PripremaPrakse_JakovSlafhauzer/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Counter } from './components/zad1/counter/counter';
+import { FilterProducts } from './components/zad2/filter-products/filter-products';
+import { UserManagement } from './components/zad5/user-management/user-management';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'counter', component: Counter },
+    { path: 'filter-products', component: FilterProducts },
+    { path: 'user-management', component: UserManagement }
+];
+
--- Frontend/PripremaPrakse_JakovSlafhauzer/src/app/app.ts
+++ Frontend/PripremaPrakse_JakovSlafhauzer/src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Counter } from './components/zad1/counter/counter';
-import { FilterProducts } from './components/zad2/filter-products/filter-products';
-import { UserManagement } from './components/zad5/user-management/user-management';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Counter, FilterProducts, UserManagement],
+  imports: [RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
